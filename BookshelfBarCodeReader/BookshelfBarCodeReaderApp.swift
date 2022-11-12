a14859fd073fbf71f1288dd57a1ca2042132b597
mnmn0 <minamino@mobamasu.com>
2022-11-12 17:54:17 +0900

    Initial Commit

---
+++ BookshelfBarCodeReader/BookshelfBarCodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookshelfBarCodeReaderApp.swift
+//  BookshelfBarCodeReader
+//
+//  Created by Shinji Minamino on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct BookshelfBarCodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
